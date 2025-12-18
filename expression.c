a2cd26f8db067f5777b5c14dfe804c6d3cff3d7c
rithwik729 <vanuchintu12345@gmail.com>
2025-12-18 09:55:05 +0530

    Add files via upload

---
+++ expression.c
@@ -0,0 +1,8 @@
+Expression 1:
+Given variables: a, b (integers), c (float), d (double) Expression: (a / b * c) - b + (a * d / 3)
+Assuming values: a = 5, b = 2, c = 3.5, d = 2.0 Computation: (5 / 2 * 3.5) - 2 + (5 * 2.0 / 3) = 10.083
+
+Expression 2:
+Given variables: i, j (integers) Expression: j = (i++) + (++i) Assuming initial value: i = 3
+Computation: j = (3++) + (++3) = 3 + 4 = 7
+
