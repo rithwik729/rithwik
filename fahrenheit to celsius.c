6c38006e1c183ae6503f72311546adf91c165f4e
rithwik729 <vanuchintu12345@gmail.com>
2025-12-18 09:52:23 +0530

    Add files via upload

---
+++ fahrenheit to celsius.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+	float F,C;
+	printf("enter fahrenheit temperature F=");
+	scanf("%f",&F);
+	C=(wqqa	5.0/9.0)*(F-32);
+	printf("celsius temperature C=%.2f\n",C);
+	return 0;
+}
+
