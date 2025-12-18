597139c519bf141b37b9122ba543dde8b94d980d
rithwik729 <vanuchintu12345@gmail.com>
2025-12-18 09:59:47 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+int main()
+{
+	int n, i, factor_count = 0; 
+	printf("Enter a number: "); 
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0) 
+		factor_count++;
+	}
+	if (factor_count ==2) 
+	printf("%d is a Prime", n); 
+	else 
+	printf("%d is not a Prime", n);
+}
+
