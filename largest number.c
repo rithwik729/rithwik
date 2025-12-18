0ab95bb3718bcbd9ef4d6c557f1fb0b33f635635
rithwik729 <vanuchintu12345@gmail.com>
2025-12-18 09:55:31 +0530

    Add files via upload

---
+++ largest number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b,c,largest;
+	printf("enter a,b,c values = ");
+	scanf("%d %d %d",&a,&b,&c);
+	largest=(a>b)?((a>c)?a:c):((b>c)?b:c);
+	printf("The Largest number = %d",largest);
+	return 0;
+}
